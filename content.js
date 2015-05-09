fcec356ca90f4832a6194296e6db624498f204f2
Wilbert Liu <wilbertliu@gmail.com>
2015-05-09 22:17:08 +0700

    Add sized images for icons

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-var hidden_icon_name_html = "<input type='hidden' value='mata-inactive.png' id='mata-icon-name' />";
+var hidden_icon_name_html = "<input type='hidden' value='mata-inactive-38.png' id='mata-icon-name' />";
 
 $("body").append(hidden_icon_name_html);
 
@@ -14,13 +14,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   // Extension is clicked
   if (request.message == "clicked_browser_action") {
     // Change the extension's icon name on hidden field
-    if ($("#mata-icon-name").val() == "mata-inactive.png") {
-      $("#mata-icon-name").val("mata-active.png");
+    if ($("#mata-icon-name").val() == "mata-inactive-38.png") {
+      $("#mata-icon-name").val("mata-active-38.png");
 
       // Activate the mata
       activate_mata();
     } else {
-      $("#mata-icon-name").val("mata-inactive.png");
+      $("#mata-icon-name").val("mata-inactive-38.png");
 
       // Deactivate the mata
       deactivate_mata();
@@ -31,7 +31,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 $("body").bind("DOMSubtreeModified", function() {
-  if ($("#mata-icon-name").val() == "mata-inactive.png") {
+  if ($("#mata-icon-name").val() == "mata-inactive-38.png") {
     deactivate_mata();
   } else {
     activate_mata();
